a9641978aa66c9532793bb108b7b6a6526eee3db
Pethaf <pethaf1@gmail.com>
2022-02-03 14:07:35 +0100

    Update script.js
    Fix embarassing errors in variable declaration

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let RegisterURL = 'https://localhost:7218/api/create'
-let form = document.getElementById("myForm")
+const RegisterURL = 'https://localhost:7218/api/create'
+const form = document.getElementById("myForm")
 
 function ShowPassword() {
-    var x = document.getElementById("password");
+    const x = document.getElementById("password");
     if (x.type === "password") {
         x.type = "text";
     } else {
@@ -12,12 +12,12 @@ function ShowPassword() {
 
 form.onsubmit = (e) => {
     e.preventDefault();
-    let firstname = e.target[0].value
-    let lastname = e.target[1].value
-    let city = e.target[2].value
-    let username = e.target[3].value
-    let email = e.target[4].value
-    let password = e.target[5].value
+    const firstname = e.target[0].value
+    const lastname = e.target[1].value
+    const city = e.target[2].value
+    const username = e.target[3].value
+    const email = e.target[4].value
+    const password = e.target[5].value
     //Below 2 rows makes sure the right gender gets sent to the database (values in HTML)
     const select = document.getElementById('Gender');
     const GenderValue = select.options[select.selectedIndex].value;
