ce980b2beddf6a2a2b56c6339ae30f0cee3dfb77
Eric Flodin <eric.flodin@aol.com>
2022-02-10 16:16:15 +0100

    Fixed bug

--- recipient.js
+++ recipient.js
@@ -2,11 +2,11 @@ const CreateRecipientURL = `https://localhost:7218/api/createRecipient`;
 const form = document.getElementById("myForm")
 
 form.onsubmit = (e) => {
-    debugger
     e.preventDefault();
-    let recipientName = n.target[0].value;
-    let recipientCity = c.target[1].value;
-    let password = e.target[2].value;
+    let recipientName = e.target[0].value;
+    let recipientCity = e.target[1].value;
+    let username = e.target[2].value;
+    let password = e.target[3].value;
     var credentials = btoa(`${username}:${password}`);
     var auth = { "Authorization" : `Basic ${credentials}` };
     
@@ -17,7 +17,7 @@ form.onsubmit = (e) => {
             method: 'POST', 
             headers: {
                 'Content-Type': 'application/json',
-                Authorization : `Basic ${credentials} ` 
+                'Authorization' : `Basic ${credentials} ` 
             }, 
             body: JSON.stringify(data)
     }) 
