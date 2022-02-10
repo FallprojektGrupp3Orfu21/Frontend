126a2b48e9b9af89f2b412d1ca2dc8a1fa617c6d
Eric Flodin <eric.flodin@aol.com>
2022-02-10 16:35:12 +0100

    Fixed some code. Now alert function works.

--- recipient.js
+++ recipient.js
@@ -1,30 +1,35 @@
-const CreateRecipientURL = `https://localhost:7218/api/createRecipient`;
+// Create recipient
+
+const CreateExpenseURL = `https://localhost:7218/api/createRecipient`;
 const form = document.getElementById("myForm")
 
 form.onsubmit = (e) => {
     e.preventDefault();
-    let recipientName = e.target[0].value;
-    let recipientCity = e.target[1].value;
-    let username = e.target[2].value;
-    let password = e.target[3].value;
-    var credentials = btoa(`${username}:${password}`);
-    var auth = { "Authorization" : `Basic ${credentials}` };
+    const recipientName = e.target[0].value;
+    const recipientCity = e.target[1].value;
+    const username = e.target[2].value;
+    const password = e.target[3].value;
+
+    const credentials = btoa(`${username}:${password}`);
+    const auth = { "Authorization" : `Basic ${credentials}` };
     
-    const data = {
-    'recipientName': recipientName
+    const data = 
+    {
+        "Name": recipientName,
+        "City": recipientCity,
     };
-    fetch(CreateRecipientURL, {
+    fetch(CreateExpenseURL, {
             method: 'POST', 
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization' : `Basic ${credentials} ` 
+                Authorization : `Basic ${credentials} ` 
             }, 
             body: JSON.stringify(data)
     }) 
         .then(response => {
             if(response.status === 200) 
             {
-                alert('Recipient Successfully Created!')
+                alert('Expense Successfully Created!')
             } 
             else
             {
